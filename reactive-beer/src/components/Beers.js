6bf56b2d0472c99d97e3104333ab79a717bef1d1
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2020-02-22 10:21:40 +0100

    Bonus

--- reactive-beer/src/components/Beers.js
+++ reactive-beer/src/components/Beers.js
@@ -7,23 +7,47 @@ import BeerComponent from "./BeerComponent";
 
 class Beers extends React.Component {
   state = {
-    beers: []
+    beers: [],
+    query: ""
   };
 
   componentDidMount() {
     axios
       .get("https://ih-beer-api.herokuapp.com/beers")
       .then(response => {
-        this.setState({ beers: response.data });
+        this.setState({ beers: response.data.splice(0, 100) });
         console.log(response.data);
       })
       .catch(err => console.error(err));
   }
 
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+
+    axios
+      .get(`https://ih-beer-api.herokuapp.com/beers/search?q=${value}`)
+      .then(response => {
+        this.setState({ beers: response.data });
+        console.log(response.data);
+      })
+      .catch(err => console.error(err));
+  };
+
   render() {
     return (
       <div className="all-beers">
         <Navbar />
+        <div className="search-bar">
+          <label>Search Beer</label>
+          <input
+            name="query"
+            type="text"
+            value={this.state.query}
+            onChange={e => this.handleChange(e)}
+          />
+        </div>
+
         <div>
           {this.state.beers.length === 0 ? (
             <h1>Loading</h1>
